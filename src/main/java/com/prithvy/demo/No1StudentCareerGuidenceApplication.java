ef8cdb3aad888d8d475a099c99a8dddcd5061fe2
Prithvi Raj <prithviraj31710@gmail.com>
2022-02-16 19:52:38 +0530

    Added a line

--- src/main/java/com/prithvy/demo/No1StudentCareerGuidenceApplication.java
+++ src/main/java/com/prithvy/demo/No1StudentCareerGuidenceApplication.java
@@ -31,20 +31,5 @@ public class No1StudentCareerGuidenceApplication {
                 .apis(RequestHandlerSelectors.basePackage("com.prithvy.demo")).build();
 
     }
-//
-//    @Bean
-//    public void connectDB()
-//    {
-//        System.out.println("Connecting to database ......" );
-////        List<ServerAddress> seeds = new ArrayList<>();
-////        seeds.add(new ServerAddress("cluster0-shard-00-00.zqniq.mongodb.net", 27017));
-////        seeds.add(new ServerAddress("cluster0-shard-00-01.zqniq.mongodb.net", 27017));
-////        seeds.add(new ServerAddress("cluster0-shard-00-02.zqniq.mongodb.net", 27017));
-////
-////        String mongoUri = "mongodb+srv://username:Password123@cluster0.zqniq.mongodb.net/CarrierGuidance?retryWrites=true&w=majority";
-////        MongoClient mongoClient =  new MongoClient(new MongoClientURI(mongoUri));
-////        System.out.println("Database Connected....");
-//        }
-
-
+//Hii Prithvy
 }
